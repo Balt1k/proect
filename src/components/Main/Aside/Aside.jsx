336e004d0bda903e84dd414f4812a8ec19b61b88
Balt1k <Tortuk19@mail.ru>
2026-05-27 22:33:41 +0300

    страница новости

--- src/components/Main/Aside/Aside.jsx
+++ src/components/Main/Aside/Aside.jsx
@@ -8,7 +8,7 @@ const Aside = () => {
             <nav className={styles.Nav}>
                 <p className={styles.head}>Панель навигации</p>
                 <ul>
-                    <li><Link to="/">Продукты и услуги</Link></li>
+                    <li><Link to="/">Продукты и услуги (главная)</Link></li>
                     <li><Link to="/about">Об авторе</Link></li>
                     <li><Link to="/contacts">Обратная связь</Link></li>
                     <li><Link to="/news">новости</Link></li>
--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Styles from '../styles/NewsPage.module.css';
+import myImage from "../assets/Images/g.gif";
 
 const NewsPage = () => {
  return (
   <div className={Styles.page}>
       <h2>Новости</h2>
-      <p>Это страница с новостями. Здесь отображаются последние события и обновления проекта.</p>
+      <p className={Styles.mainText}>Это страница с новостями. На данный момент здесь пусто :(</p>
+      <p className={Styles.secondText}>Однако мы активно готовим для вас новые истории с нашими счастливыми клиентами.</p>
+      <div className={Styles.imageContainer}>
+                <img src={myImage} alt='анимация' className={Styles.image} />
+              </div>
     </div>
  );
 };
